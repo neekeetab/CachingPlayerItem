78d5da7de1b6dafb529788536697f75640e6d6fe
alemmi <alessandro.emmi@funambol.com>
2019-06-11 18:14:08 +0200

    BufferMode: disk - implementation

--- CachingPlayerItem.swift
+++ CachingPlayerItem.swift
@@ -30,9 +30,89 @@ fileprivate extension URL {
     /// Is called on downloading error.
     @objc optional func playerItem(_ playerItem: CachingPlayerItem, downloadingFailedWith error: Error)
     
+    /// - BufferMode: disk
+    
+    /// Invoked every time a new chunk of data is available for caching
+    @objc optional func playerItem(_ playerItem: CachingPlayerItem, hasDataAvailableForCaching data: Data, bytesDownloaded: Int, outOf bytesExpected: Int, didCache: (() -> Void)?, didFailCaching: (() -> Void)?)
+    
+    /// Invoked to request data to be provided to the asset data request
+    @objc optional func playerItem(_ playerItem: CachingPlayerItem, needsStreamingData startByte: Int, endByte: Int) -> Data?
+    
+}
+
+enum BufferMode {
+    case memory
+    case disk
 }
 
 open class CachingPlayerItem: AVPlayerItem {
+    
+    // Provides the opportunity to avoid loading potentially large amounts of data into memory
+    // Requires that the following methods are implemented by the CachingPlayerItemDelegate object
+    
+    // - playerItem(_ playerItem:hasDataAvailableForCaching:bytesDownloaded:outOf:didCache:didFailCaching:)
+    // - playerItem(_ playerItem:needsStreamingData:endByte: Int) -> Data?
+    
+    // Provide your own implementation of the first method in order to progressively cache the downloading resource to disk
+    // Provide your own implementation of the second method in order to read and provide the data requested for streaming directly from the partially cached file
+    
+    // maxBufferSize is set to 1MB by default (it can be customized) - specifies the largest amount of data to be read from the cached resource and loaded to memory
+    class DiskBufferResourceLoaderDelegate: ResourceLoaderDelegate {
+        let maxBufferSize: Int
+        
+        init(maxBufferSize: Int = 1048576) {
+            self.maxBufferSize = maxBufferSize
+        }
+        
+        var totalBytesSize: Int = 0
+        var bytesDownloadedSoFar: Int = 0
+        var bytesCachedSoFar: Int = 0
+        
+        override func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+            totalBytesSize = Int(dataTask.countOfBytesExpectedToReceive)
+            super.urlSession(session, dataTask: dataTask, didReceive: response, completionHandler: completionHandler)
+        }
+        
+        override func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+            processPendingRequests()
+            bytesDownloadedSoFar += data.count
+            
+            // Every new chunk of data downloaded is made available for caching
+            owner?.delegate?.playerItem?(owner!, hasDataAvailableForCaching: data, bytesDownloaded: bytesDownloadedSoFar, outOf: totalBytesSize, didCache: {
+                self.bytesCachedSoFar += data.count
+            }, didFailCaching: {
+                // If there's an error caching the data, we don't have a reliable data source anymore - cancel dataTask
+                dataTask.cancel()
+            })
+            
+        }
+        
+        override func haveEnoughDataToFulfillRequest(_ dataRequest: AVAssetResourceLoadingDataRequest) -> Bool {
+            let requestedOffset = Int(dataRequest.requestedOffset)
+            let requestedLength = dataRequest.requestedLength
+            let currentOffset = Int(dataRequest.currentOffset)
+            
+            // Is there enough data cached to fulfill the request?
+            guard bytesCachedSoFar > currentOffset else { return false }
+            
+            // The amount of bytes to load in memory and provide to the request is the smallest possible
+            // In any case it's never bigger than maxBufferSize
+            let bytesToRespond = min(bytesCachedSoFar - currentOffset, requestedLength, maxBufferSize)
+            
+            let firstByte = currentOffset
+            let lastByte = currentOffset + bytesToRespond
+            
+            // Read data from disk and provide it to the dataRequest
+            guard let data = owner?.delegate?.playerItem?(owner!, needsStreamingData: firstByte, endByte: lastByte) else { return false }
+            dataRequest.respond(with: data)
+            
+            return bytesCachedSoFar >= requestedLength + requestedOffset
+        }
+        
+        func didCompleteDownload() -> Bool {
+            return (bytesCachedSoFar == bytesDownloadedSoFar) && (bytesCachedSoFar == totalBytesSize)
+        }
+    }
     
     class ResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate, URLSessionDelegate, URLSessionDataDelegate, URLSessionTaskDelegate {
         
@@ -70,8 +150,16 @@ open class CachingPlayerItem: AVPlayerItem {
         func startDataRequest(with url: URL) {
             let configuration = URLSessionConfiguration.default
             configuration.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+            
             session = URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-            session?.dataTask(with: url).resume()
+            var request = URLRequest(url: url)
+            
+            // some servers may send the wrong Content-Length value for “Content-Encoding: gzip” content
+            let headers = ["Accept-Encoding" : "*/*"]
+            request.allHTTPHeaderFields = headers
+            
+            let task = session?.dataTask(with: request)
+            task?.resume()
         }
         
         func resourceLoader(_ resourceLoader: AVAssetResourceLoader, didCancel loadingRequest: AVAssetResourceLoadingRequest) {
@@ -168,10 +256,11 @@ open class CachingPlayerItem: AVPlayerItem {
         
     }
     
-    fileprivate let resourceLoaderDelegate = ResourceLoaderDelegate()
+    fileprivate let resourceLoaderDelegate: ResourceLoaderDelegate
     fileprivate let url: URL
     fileprivate let initialScheme: String?
     fileprivate var customFileExtension: String?
+    fileprivate let bufferMode: BufferMode
     
     weak var delegate: CachingPlayerItemDelegate?
     
@@ -190,7 +279,7 @@ open class CachingPlayerItem: AVPlayerItem {
     
     /// Override/append custom file extension to URL path.
     /// This is required for the player to work correctly with the intended file type.
-    init(url: URL, customFileExtension: String?) {
+    init(url: URL, customFileExtension: String?, bufferMode: BufferMode = .memory, maxBufferSize: Int? = nil) {
         
         guard let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
             let scheme = components.scheme,
@@ -200,11 +289,23 @@ open class CachingPlayerItem: AVPlayerItem {
         
         self.url = url
         self.initialScheme = scheme
+        self.bufferMode = bufferMode
         
         if let ext = customFileExtension {
             urlWithCustomScheme.deletePathExtension()
             urlWithCustomScheme.appendPathExtension(ext)
             self.customFileExtension = ext
+        }
+        
+        switch bufferMode {
+        case .disk:
+            if let customBufferSize = maxBufferSize {
+                resourceLoaderDelegate = DiskBufferResourceLoaderDelegate(maxBufferSize: customBufferSize)
+            } else {
+                resourceLoaderDelegate = DiskBufferResourceLoaderDelegate()
+            }
+        case .memory:
+            resourceLoaderDelegate = ResourceLoaderDelegate()
         }
         
         let asset = AVURLAsset(url: urlWithCustomScheme)
@@ -228,6 +329,9 @@ open class CachingPlayerItem: AVPlayerItem {
         
         self.url = fakeUrl
         self.initialScheme = nil
+        self.bufferMode = .memory
+        
+        resourceLoaderDelegate = ResourceLoaderDelegate()
         
         resourceLoaderDelegate.mediaData = data
         resourceLoaderDelegate.playingFromData = true
@@ -263,6 +367,12 @@ open class CachingPlayerItem: AVPlayerItem {
     }
     
     deinit {
+        // If an item gets deallocated before it was completely downloaded it means the download was cancelled
+        // We may want to notify it - e.g. to clear a partially cached file
+        if bufferMode == .disk, let resourceDelegate = resourceLoaderDelegate as? DiskBufferResourceLoaderDelegate, !resourceDelegate.didCompleteDownload() {
+            let error = NSError(domain: NSURLErrorDomain, code: NSURLErrorCancelled, userInfo: nil)
+            self.delegate?.playerItem?(self, downloadingFailedWith: error)
+        }
         NotificationCenter.default.removeObserver(self)
         removeObserver(self, forKeyPath: "status")
         resourceLoaderDelegate.session?.invalidateAndCancel()
